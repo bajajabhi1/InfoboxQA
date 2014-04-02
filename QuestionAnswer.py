c0bb753d76e717eda1c8e1dfa2eba06a5d5f0201
rafica <rafica.ar@gmail.com>
2014-04-01 23:57:17 -0400

    QA updated

--- QuestionAnswer.py
+++ QuestionAnswer.py
@@ -1,6 +1,45 @@
 import re
+import urllib
+import urllib2
+import json
 
-def questionAnswering:
-    query = "Who created microsoft?"
+def questionAnswering():
+    query = "Who created lord of the rings?"
     creationObj = re.match(r'Who created (.*)\?', query)
-    print creationObj.group(1)
+    creation = creationObj.group(1)
+
+    api_key = "AIzaSyAIB0OSE8CQ4yETwSFefsMh0_k1RMnT2k4"
+    mqlQuery = [{"type": "/book/written_work", "author": [],"name~=": creation, "a:name": None }]
+    params = {
+            'query':json.dumps(mqlQuery),
+            'key': api_key
+            }
+    
+    service_url = 'https://www.googleapis.com/freebase/v1/mqlread'
+
+    url = service_url + '?' + urllib.urlencode(params)
+    response = json.loads(urllib.urlopen(url).read())
+
+    #print response['result']
+    for entry in response['result']:
+        if entry['author']:
+            print entry['a:name']+' written by ' + ', '.join(entry['author'])
+
+    mqlQuery = [{"type": "/organization/organization", "founders": [], "name~=": "microsoft", "a:name": None}]
+    params = {
+            'query':json.dumps(mqlQuery),
+            'key': api_key
+            }
+    url = service_url + '?' + urllib.urlencode(params)
+    response = json.loads(urllib.urlopen(url).read())
+
+    #print response['result']
+    for entry in response['result']:
+        if entry['founders']:
+            print entry['a:name']+' founded by ' + ', '.join(entry['founders'])
+
+
+    
+    
+    
+questionAnswering()
